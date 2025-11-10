e8ef105afbabe7171c0a6dec7cb179a5f4c3654b
Christian Lapinig <clapinig@adobe.com>
2025-11-10 08:06:57 -0800

    add config module

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"os"
+	"fmt"
 
+	"github.com/ChristianLapinig/tmdb-cli/config"
 	"github.com/ChristianLapinig/tmdb-cli/program"
 )
 
 func main() {
-	accessToken := os.Getenv("TMDB_API_READ_ACCESS_TOKEN")
-	program.Program(accessToken)
+	config, err := config.InitializeConfig()
+	if err != nil {
+		fmt.Println("ERROR:", err.Error())
+	}
+	program.Program(config.AccessToken)
 }
